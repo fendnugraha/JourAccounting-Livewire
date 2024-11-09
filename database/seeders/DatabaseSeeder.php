5140db407baf52794a90c1b416f34dab8b8e5af9
fendnugraha <fendnugraha92@gmail.com>
2024-11-09 23:53:27 +0700

    initial app

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,14 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Role;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Contact;
+use App\Models\Warehouse;
 use Illuminate\Database\Seeder;
+use Database\Seeders\AccountSeeder;
+use Database\Seeders\ChartOfAccountSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +21,43 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
+            'name' => 'fend',
+            'email' => 'fend@jour.com',
+        ]);
+
+        User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        Warehouse::create([
+            'code' => 'HQT',
+            'name' => 'HEADQUARTER',
+            'address' => 'Bandung, Jawa Barat, ID, 40375',
+            'chart_of_account_id' => 1
+        ]);
+
+        Role::create([
+            'user_id' => 1,
+            'warehouse_id' => 1,
+            'role' => 'Administrator'
+        ]);
+
+        Role::create([
+            'user_id' => 2,
+            'warehouse_id' => 1,
+            'role' => 'Administrator'
+        ]);
+
+        Contact::create([
+            'name' => 'General',
+            'type' => 'Customer',
+            'Description' => 'General Customer',
+        ]);
+
+        $this->call([
+            AccountSeeder::class,
+            ChartOfAccountSeeder::class
         ]);
     }
 }

9db90b42942935a7f67f6c4587dabba38187b730
Fend Nugraha <fendnugraha@Fends-Book.local>
2025-11-17 11:22:02 +0700

    add product edit

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -30,12 +30,12 @@ new class extends Component
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
                     <x-nav-link :href="route('transaction')" :active="request()->routeIs(['transaction*'])"
                         wire:navigate>
                         {{ __('Transaction') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
+                        {{ __('Report') }}
                     </x-nav-link>
                     @can('admin')
                     <x-nav-link :href="route('summary')" :active="request()->routeIs(['summary*'])" wire:navigate>
